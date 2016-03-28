b0697378453c8dfa579351acd15b482ebc476295
Josh English <josh@joshenglish.com>
2016-03-28 15:35:45 -0500

    removing development.sql & devlopment.sqlite

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -5,17 +5,22 @@ class UsersController < ApplicationController
 
   # Registration Page
   # Form Names from Params: username, email, password
-  post '/register'
-    password = BCrypt::Password.create(params[:password])
-    @new_user = Users.create username: params[:username], email: params[:email], password: password
-    "You have registered"
+  get '/create' do
+    password = BCrypt::Password.create('test')
+    # @new_user = Users.create username: params[:username], email: params[:email], password: password
+    @new_user = User.create username: 'testjosh', email: 'test@joshenglish.com', password: password
+    "You have registered {@new_user.username}"
+  end
+
+  get '/whois' do
+    @all_users = User.all
+    @all_users.to_s
   end
 
   # Login Page
   # Form Name from Params: username, password
   post '/login' do
     user = User[username: params[:username]]
-
     stored_password = BCrypt::Password.new(user.password)
     if user && compare_to == params[:password]
       session[:logged_in] = true
@@ -23,6 +28,7 @@ class UsersController < ApplicationController
       "Welcome back #{user.username}"
     else
       "You have entered the wrong email & password combination"
+    end
   end
 
   # Logout Page
