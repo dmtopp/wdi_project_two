1c0ef7cdbec57de053489d2d060141fd4c552c51
Daniel Topp <dmtopp@gmail.com>
2016-03-29 15:00:18 -0500

    now grabs locations close to where the user is and pins them and displays the first five in a list

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,56 +1,28 @@
 var map = new GMaps({
+  zoom: 18,
   div: '#map',
   lat: 41.890551,
   lng: -87.626850
 });
 
 $('#geolocate').click(function(){
-
+  map.removeMarkers();
   GMaps.geolocate({
     success: function(position) {
-      lat = position.coords.latitude;
-      lng = position.coords.longitude;
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
 
       map.setCenter(lat, lng);
 
       map.addMarker({
         lat: lat,
         lng: lng,
-        title: 'GA-Chicago',
+        label: 'You! (ish)',
         infoWindow: {
-          content: '<img src="https://media.giphy.com/media/fZk0FD0wxQpb2/giphy.gif">'
+          content: '<h4>Your position!</h4>'
         }
       })
-
-      $.ajax({
-        method: 'post',
-        url: '/reviews',
-        data: {
-          "lat": lat,
-          "lng": lng
-        },
-        success: function(data){
-          console.log(data);
-        },
-        failure: function(err){
-          console.log(err);
-        }
-      });
-
-
-
-      // $.ajax({
-      //   method: 'get',
-      //   url: 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat +',' + lng,
-      //   success: function(data){
-      //     console.log(data.results[0].formatted_address);
-      //   },
-      //   failure: function(err){
-      //     console.log(err);
-      //   }
-      // });
-
-
+      sendLocation(lat,lng);
     },
     error: function(error) {
       console.log('Geolocation failed: '+error.message);
@@ -66,9 +38,7 @@ $('#geolocate').click(function(){
 })
 
 $('#address_search').click(function(e){
-
-  console.log('click addy');
-
+  map.removeMarkers();
   GMaps.geocode({
     address: $('#address').val().trim(),
     callback: function(results, status) {
@@ -77,27 +47,65 @@ $('#address_search').click(function(e){
         var lng = results[0].geometry.location.lng();
         map.setCenter(lat, lng);
         map.addMarker({
+          infoWindow: {
+            content: '<h4>Your position!</h4>'
+          },
+          label: 'You! (ish)',
           lat: lat,
           lng: lng
         });
-
-        $.ajax({
-          method: 'post',
-          url: '/reviews',
-          data: {
-            "lat": lat,
-            "lng": lng
-          },
-          success: function(data){
-            console.log(data);
-          },
-          failure: function(err){
-            console.log(err);
-          }
-        });
+        sendLocation(lat, lng);
       } else {
-        console.log('wtf')
+        console.log(status);
       }
     }
   });
 })
+
+
+function sendLocation(lat,lng){
+  $.ajax({
+    method: 'post',
+    url: '/reviews',
+    data: {
+      "lat": lat,
+      "lng": lng
+    },
+    success: function(data){
+      $('#list-results').html('');
+      var array_of_places = JSON.parse(data);
+      array_of_places.forEach(function(place){
+        placeMarker(place);
+        if (array_of_places.indexOf(place) < 5) addToList(place);
+      })
+    },
+    failure: function(err){
+      console.log(err);
+    }
+  });
+}
+
+
+function placeMarker(place){
+  var self = place;
+  self.avgRating = 5;
+  self.numReviews = 10;
+  map.addMarker({
+    lat: self.lat,
+    lng: self.lng,
+    title: self.place_name,
+    infoWindow: {
+      content:  '<h4>' + self.place_name + '</h4>' +
+                '<p>Average Rating: ' + self.avgRating + '/5</p>' +
+                '<small>' + self.numReviews + ' people reviewed this location</small>' +
+                '<p><a href="#">Add a review</a></p>'
+    }
+  })
+}
+
+function addToList(place){
+  $('#list-results').append('<div class="results-item">' +
+                            '<li>' + place.place_name + ' ' + place.avgRating +'/5</li>' +
+                            '<li><small><a href="#">Write a review for this location</a></small></li>' +
+                            '</div>');
+}
