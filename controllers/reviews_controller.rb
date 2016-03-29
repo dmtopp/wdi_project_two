98b21ceaf7f2a961c614da9bfe966cb753598411
Josh English <josh@joshenglish.com>
2016-03-29 09:57:33 -0500

    changes to the review controller - incomplete

--- controllers/reviews_controller.rb
+++ controllers/reviews_controller.rb
@@ -1,11 +1,22 @@
 class ReviewsController < ApplicationController
 
   # Displays Top 20 Locations via the spot_by_query.  Ideally want to use long and lat for @client.spot(long, lat, options = {})
-  get '/' do
+  post '/' do
     @client = GooglePlaces::Client.new(ENV["API_KEY"])
-    location_info = @client.spots_by_query('food near 444 Wabash Ave Chicago IL', :radius=>500)
+    #location_info = @client.spot(params[:long],params[:lat], type=>'establishment')
+    location_info = @client.spots('41.890651','-87.626968', :type=>'establishment', :exclude=>'neighborhood',:rankby=>'distance')
     erb :display, locals: { location_info: location_info }
   end
+
+
+  get '/postreview' do
+    if session[:logged_in] === true
+      erb :reviewform
+
+
+
+
+
 
 
 end
