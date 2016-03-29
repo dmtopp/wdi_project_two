c52477c98e4dc2ae91255839f034f2f2a69d0d30
Josh English <josh@joshenglish.com>
2016-03-29 11:06:28 -0500

    Updating with changes to backend

--- controllers/reviews_controller.rb
+++ controllers/reviews_controller.rb
@@ -3,15 +3,30 @@ class ReviewsController < ApplicationController
   # Displays Top 20 Locations via the spot_by_query.  Ideally want to use long and lat for @client.spot(long, lat, options = {})
   post '/' do
     @client = GooglePlaces::Client.new(ENV["API_KEY"])
-    #location_info = @client.spot(params[:long],params[:lat], type=>'establishment')
-    location_info = @client.spots('41.890651','-87.626968', :type=>'establishment', :exclude=>'neighborhood',:rankby=>'distance')
-    erb :display, locals: { location_info: location_info }
+    location_info = @client.spots(params[:lat], params[:lng], :type=>'establishment', :exclude=>'neighborhood',:rankby=>'distance')
+    # erb :display, locals: { location_info: location_info }
+    puts params
+    @all_locations = []
+    location_info.each do |item|
+      location = {
+        "place_name": item.name,
+        "lat": item.lat.to_s,
+        "lng": item.lng.to_s,
+        "place_id": item.place_id
+      }
+      @all_locations.push(location)
+    end
+    @all_locations.to_json
   end
 
 
   get '/postreview' do
     if session[:logged_in] === true
       erb :reviewform
+    end
+  end
+
+
 
 
 
