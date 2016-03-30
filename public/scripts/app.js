8ea1c75c77b2152bd48d8a08865a99ae49f61105
Daniel Topp <dmtopp@gmail.com>
2016-03-30 11:46:09 -0500

    changed from bootstrap to skeleton and added some stars

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -164,3 +164,11 @@ function addToList(place){
     //                           '</div>');
   });
 }
+
+var logID = 'log',
+  log = $('<div id="'+logID+'"></div>');
+$('body').append(log);
+  $('[type*="radio"]').change(function () {
+    var me = $(this);
+    log.html(me.attr('value'));
+  });
